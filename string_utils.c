31f6f69796aae7967bdcfd937f7c105d5c4fc0ad
Aosgos <59194295+Aosgos@users.noreply.github.com>
2022-10-22 21:52:07 +0100

    Create

---
+++ string_utils.c
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int get_size(const char *format int *i);
+
+#endif /*_MAIN.H_*/
